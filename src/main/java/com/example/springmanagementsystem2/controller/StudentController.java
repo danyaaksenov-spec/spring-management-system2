6c65a7ef42a422ed9601717688527baeef2c7701
Danny <81240837+danyaaksenov-spec@users.noreply.github.com>
2021-10-09 18:36:37 +0300

    finished project

--- src/main/java/com/example/springmanagementsystem2/controller/StudentController.java
+++ src/main/java/com/example/springmanagementsystem2/controller/StudentController.java
@@ -4,9 +4,7 @@ import com.example.springmanagementsystem2.entity.Student;
 import com.example.springmanagementsystem2.service.StudentService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class StudentController {
@@ -34,6 +32,33 @@ public class StudentController {
     @PostMapping("/students")
     public String saveStudent(@ModelAttribute("student") Student student) {
         studentService.saveStudent(student);
+        return "redirect:/students";
+    }
+
+    @GetMapping("/students/edit/{id}")
+    public String editStudentForm(@PathVariable Long id, Model model) {
+    model.addAttribute("student", studentService.getStudentById(id));
+    return "edit_student";
+    }
+
+    @PostMapping("/students/{id}")
+    public String updateStudent(@PathVariable Long id, @ModelAttribute("student") Student student, Model model) {
+        //get students from db by id
+        Student existingStudent = studentService.getStudentById(id);
+        existingStudent.setId(id);
+        existingStudent.setFirstName(student.getFirstName());
+        existingStudent.setLastName(student.getLastName());
+        existingStudent.setEmail(student.getEmail());
+
+        //save existing students
+        studentService.updateStudent(student);
+        return "redirect:/students";
+    }
+
+    //handler method to handle delete student request
+    @GetMapping("/students/{id}")
+    public String deleteStudent(@PathVariable Long id) {
+        studentService.deleteStudentById(id);
         return "redirect:/students";
     }
 }
